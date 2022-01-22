a053b80c0e7fea0ef5d5fdd8244ec7221408fd04
Sandro Bistene <sandrosalasfilho3@gmail.com>
2022-01-21 22:14:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.getElementById('saibamais');
+
+botao.addEventListener('click' , function(){
+  alert('Botão clicado!');
+  console.log('Seja bem vindo a ganhar com o Milton!')
+});
